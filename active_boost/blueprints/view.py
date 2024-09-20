3c41864a72e375b2f035704d3bcd0a967644a8a3
na-stewart <na.stewart365@gmail.com>
2024-09-20 10:47:49 -0400

    View setup

--- active_boost/blueprints/view.py
+++ active_boost/blueprints/view.py
@@ -1,21 +1,12 @@
 from sanic import Blueprint
 
 api_models = [
-    "gpt_orchestrator.blueprints.chat.models",
-    "gpt_orchestrator.blueprints.document.models",
-    "gpt_orchestrator.blueprints.system.models",
-    "gpt_orchestrator.blueprints.index.models",
+    "active_boost.blueprints.econ.models"
+    "active_boost.blueprints.social.models"
     "sanic_security.models",
 ]
 
 api = Blueprint.group(
-    chat_bp,
-    security_bp,
-    account_bp,
-    roles_bp,
-    document_bp,
-    system_bp,
-    index_bp,
     version=1,
     version_prefix="/api/v",
 )
