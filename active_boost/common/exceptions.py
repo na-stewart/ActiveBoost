39b644902a0aa10d2a626e38802442af7aaf5e5b
na-stewart <na.stewart365@gmail.com>
2024-11-07 11:54:43 -0500

    Syntax revision

--- active_boost/common/exceptions.py
+++ active_boost/common/exceptions.py
@@ -57,5 +57,7 @@ class AnonymousUserError(ActiveBoostError):
     """
 
     def __init__(self):
-        super().__init__("Login required, please do so at "
-                         "https://activeboost.na-stewart.com/api/v1/security/login", 401)
+        super().__init__(
+            "Login required, please do so at https://activeboost.na-stewart.com/api/v1/security/login",
+            401,
+        )
